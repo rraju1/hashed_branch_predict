5b90e98bfff65cc5bba42f05665b1ad29c750647
Ravi S Raju <rraju2@provolone>
2017-12-05 09:46:48 -0600

    results for tage

---
+++ results_ltage/436.cactusADM/0/job.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+    /research/rraju2/ece752/gem5/build/ARM/gem5.opt /research/rraju2/ece752/gem5/configs/example/se.py --cpu-type=O3_ARM_v7a_3 --caches --l2cache --mem-size=2GB --cpu-clock=2GHz --sys-clock=2GHz --maxinsts=1000000000 --fast-forward=1000000000 --cmd=436.cactusADM --options="benchADM.par" --output=436.cactusADM.stdout --errout=436.cactusADM.stderr 
