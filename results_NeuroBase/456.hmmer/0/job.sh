0da9873d8411d8d3200cc425d3b9622d65df6511
Ravi S Raju <rraju2@provolone>
2017-12-05 19:05:36 -0600

    results_base

---
+++ results_NeuroBase/456.hmmer/0/job.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+    /research/rraju2/ece752/gem5/build/ARM/gem5.opt /research/rraju2/ece752/gem5/configs/example/se.py --cpu-type=O3_ARM_v7a_3 --caches --l2cache --mem-size=2GB --cpu-clock=2GHz --sys-clock=2GHz --maxinsts=1000000000 --fast-forward=1000000000 --cmd=456.hmmer --options="--fixed 0 --mean 425 --num 85000 --sd 300 --seed 0 leng100.hmm" --output=456.hmmer.stdout --errout=456.hmmer.stderr 
