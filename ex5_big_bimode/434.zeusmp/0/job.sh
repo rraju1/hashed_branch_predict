ba7db0b5ae8664d804edbf1ca346c993edb274cb
Ravi S Raju <rraju2@havarti>
2017-12-08 10:22:19 -0600

    results for bimode and base

---
+++ ex5_big_bimode/434.zeusmp/0/job.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+    /research/rraju2/ece752/gem5/build/ARM/gem5.opt /research/rraju2/ece752/gem5/configs/example/se.py --cpu-type=ex5_big --fast-forward=1000000000 --caches --l2cache --mem-size=2GB --cpu-clock=2GHz --sys-clock=2GHz --maxinsts=1000000000 --cmd=434.zeusmp --output=434.zeusmp.stdout --errout=434.zeusmp.stderr 
---
+++ ex5_big_bimode/453.povray/0/job.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+    /research/rraju2/ece752/gem5/build/ARM/gem5.opt /research/rraju2/ece752/gem5/configs/example/se.py --cpu-type=ex5_big --fast-forward=1000000000 --caches --l2cache --mem-size=2GB --cpu-clock=2GHz --sys-clock=2GHz --maxinsts=1000000000 --cmd=453.povray --options="SPEC-benchmark-train.ini" --output=453.povray.stdout --errout=453.povray.stderr 
